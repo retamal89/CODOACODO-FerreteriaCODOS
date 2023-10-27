73e0ec9d93acd37a5dfd797016f41d46d1604758
Jose Luis Retamal Rojas <retamal.jl.1989@gmail.com>
2023-10-27 02:15:05 -0300

    arreglos JS

--- main.js
+++ main.js
@@ -1,59 +1,41 @@
-// Obtén el formulario por su ID
-const formulario = document.querySelector('.formulario');
-
-// Agrega un evento de envío al formulario
-formulario.addEventListener('submit', function (event) {
-  // Variables para almacenar los errores
-  let errores = false;
-
-  // Obtén los campos del formulario
-  const nombre = document.getElementById('nombre');
-  const apellido = document.getElementById('apellido');
-  const email = document.getElementById('email');
-  const telefono = document.getElementById('telefono');
-  const consulta = document.getElementById('consulta');
-
-  // Valida el campo Nombre
-  if (nombre.value === '') {
-    alert('Por favor, ingrese su nombre.');
-    errores = true;
-  }
-
-  // Valida el campo Apellido
-  if (apellido.value === '') {
-    alert('Por favor, ingrese su apellido.');
-    errores = true;
-  }
-
-  // Valida el campo Email
-  if (email.value === '') {
-    alert('Por favor, ingrese su correo electrónico.');
-    errores = true;
-  } else if (!isValidEmail(email.value)) {
-    alert('Por favor, ingrese una dirección de correo electrónico válida.');
-    errores = true;
-  }
-
-  // Valida el campo Teléfono
-  if (telefono.value === '') {
-    alert('Por favor, ingrese su teléfono.');
-    errores = true;
-  }
-
-  // Valida el campo Consulta
-  if (consulta.value === '') {
-    alert('Por favor, ingrese su consulta.');
-    errores = true;
-  }
-
-  // Si hay errores, evita que el formulario se envíe
-  if (errores) {
-    event.preventDefault();
-  }
-});
-
 // Función para validar una dirección de correo electrónico
 function isValidEmail(email) {
-  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-  return emailRegex.test(email);
-}
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailRegex.test(email);
+  }
+  
+  // Función para mostrar mensajes de error
+  function mostrarError(mensaje) {
+    const erroresContainer = document.getElementById('errores-container');
+    const errorParagraph = document.createElement('p');
+    errorParagraph.textContent = mensaje;
+    erroresContainer.appendChild(errorParagraph);
+  }
+  
+  // Función para obtener el valor del dólar oficial
+  function obtenerValorDolar() {
+    const dolarAmountElement = document.getElementById('dolar-amount');
+    const apiUrl = 'https://dolarapi.com/v1/dolares/oficial';
+  
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        dolarAmountElement.textContent = data.compra.toFixed(2);
+      })
+      .catch(error => {
+        console.error('Error al obtener el valor del dólar:', error);
+        dolarAmountElement.textContent = 'Error al obtener el valor';
+      });
+  }
+  
+  // Función para validar el formulario
+  function validarFormulario(event) {
+  }
+  
+  // Agrega un evento de envío al formulario
+  const formulario = document.querySelector('.formulario');
+  formulario.addEventListener('submit', validarFormulario);
+  
+  // Llama a la función para obtener el valor del dólar cuando se carga la página
+  window.onload = obtenerValorDolar;
+  

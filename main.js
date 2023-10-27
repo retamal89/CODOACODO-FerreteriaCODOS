1cc496db2e6e7400e9969b74885d1f21b330e1b7
Jose Luis Retamal Rojas <retamal.jl.1989@gmail.com>
2023-10-27 01:29:07 -0300

    agregado validacion javascript

--- main.js
+++ main.js
@@ -1,20 +1,59 @@
-// Función para obtener el valor del dólar desde una API
-function obtenerValorDolar() {
-    // Reemplaza la siguiente URL con la API que deseas utilizar
-    const apiUrl = 'https://dolarapi.com/v1/dolares/oficial'; // Cambia esta URL por la API real que prefieras
+// Obtén el formulario por su ID
+const formulario = document.querySelector('.formulario');
 
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            // Actualiza el valor del dólar en el elemento HTML con el ID "dolar-amount"
-            const dolarAmount = data.valor; // Asume que la API devuelve el valor del dólar en "valor"
-            const dolarAmountElement = document.getElementById('dolar-amount');
-            dolarAmountElement.textContent = `$${dolarAmount.toFixed(2)}`;
-        })
-        .catch(error => {
-            console.error('Error al obtener el valor del dólar', error);
-        });
+// Agrega un evento de envío al formulario
+formulario.addEventListener('submit', function (event) {
+  // Variables para almacenar los errores
+  let errores = false;
+
+  // Obtén los campos del formulario
+  const nombre = document.getElementById('nombre');
+  const apellido = document.getElementById('apellido');
+  const email = document.getElementById('email');
+  const telefono = document.getElementById('telefono');
+  const consulta = document.getElementById('consulta');
+
+  // Valida el campo Nombre
+  if (nombre.value === '') {
+    alert('Por favor, ingrese su nombre.');
+    errores = true;
+  }
+
+  // Valida el campo Apellido
+  if (apellido.value === '') {
+    alert('Por favor, ingrese su apellido.');
+    errores = true;
+  }
+
+  // Valida el campo Email
+  if (email.value === '') {
+    alert('Por favor, ingrese su correo electrónico.');
+    errores = true;
+  } else if (!isValidEmail(email.value)) {
+    alert('Por favor, ingrese una dirección de correo electrónico válida.');
+    errores = true;
+  }
+
+  // Valida el campo Teléfono
+  if (telefono.value === '') {
+    alert('Por favor, ingrese su teléfono.');
+    errores = true;
+  }
+
+  // Valida el campo Consulta
+  if (consulta.value === '') {
+    alert('Por favor, ingrese su consulta.');
+    errores = true;
+  }
+
+  // Si hay errores, evita que el formulario se envíe
+  if (errores) {
+    event.preventDefault();
+  }
+});
+
+// Función para validar una dirección de correo electrónico
+function isValidEmail(email) {
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  return emailRegex.test(email);
 }
-
-// Llama a la función para obtener el valor del dólar cuando se cargue la página
-obtenerValorDolar();
